410c2a09522df7e40eeab993d69f73ae47787fcb
gox <gox.roche@gmail.com>
2020-08-08 22:36:13 +0200

    Find candidates by 3x3 boxes

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -124,15 +124,49 @@ fn clean_candidates_by_lines_columns(sudoku_grid: &mut Vec<Vec<Cell>>) {
     }
 }
 
+fn clean_candidates_by_boxes(sudoku_grid: &mut Vec<Vec<Cell>>) {
+    let mut j = 0;
+    while j < 9 {
+        let mut i = 0;
+        while i < 9 {
+            let mut box_values = Vec::new();
+            for x in i..i+3 {
+                for y in j..j+3 {
+                    let cell = &mut sudoku_grid[y][x];
+                    if cell.value != 0 {
+                        box_values.push(cell.value);
+                    }
+                }
+            }
+            for x in i..i+3 {
+                for y in j..j+3 {
+                    let cell = &mut sudoku_grid[y][x];
+                    if cell.value == 0 {
+                        for c in 0..box_values.len() {
+                            cell.candidates.retain(|x| *x != box_values[c]);
+                        }
+                        if cell.candidates.len() == 1 {
+                            println!("Value {} found at x:{},y:{}", cell.candidates[0], cell.x, cell.y);
+                            cell.value = cell.candidates[0];
+                        }
+                    }
+                }
+            }
+           i = i + 3;
+        }
+        j = j + 3;
+    }
+}
+
 pub fn resolve_sudoku_grid(sudoku_grid: &Vec<Vec<Cell>>) -> Vec<Vec<Cell>> {
     let mut resolved_sudoku_grid: Vec<Vec<Cell>> = clone_sudoku_grid(&sudoku_grid);
+    print_sudoku_grid(&resolved_sudoku_grid);
+
+    println!("\nClean candidates by lines and columns:");
     clean_candidates_by_lines_columns(&mut resolved_sudoku_grid);
 
-    /*for y in 0..sudoku_grid.len() {
-        for x in 0..sudoku_grid[y].len() {
-            println!("{:?} ", sudoku_grid[y][x]);
-        }
-    }*/
-
+    println!("\nClean candidates by 3x3 boxes:");
+    clean_candidates_by_boxes(&mut resolved_sudoku_grid);
+    println!("");
     resolved_sudoku_grid
 }
